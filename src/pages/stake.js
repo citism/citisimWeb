17f293962d94559666218867da1c4a169f7d1c1e
mijin <>
2022-04-20 22:06:57 +0900

    add:font

--- src/pages/stake.js
+++ src/pages/stake.js
@@ -7,25 +7,24 @@ import classNames from "classnames";
 export default function Title() {
   return (
     <>
-      <div
-        className={
-          "text-white w-screen h-screen flex items-center justify-center"
-        }
-      >
-        <div className="stake_left">
-          <div className="title">Step1</div>
-          <div className="text-white">Get the stake you want</div>
-        </div>
-        <div className="stake_center">
-          <div className="title">Step2</div>
-          <div className="text-white">
-            Proceed with delegation to citism.dev according to the staking
-            guide.
+      <div className="stake_container">
+        <div className="stake_title">How To Stake</div>
+        <div className={"step_container"}>
+          <div className="stake_left">
+            <div className="title">Step1</div>
+            <div className="text-white">Get the stake you want</div>
           </div>
-        </div>
-        <div className="stake_right">
-          <div className="title">Step3</div>
-          <div className="text-white">Get rewarded or re-stake again.</div>
+          <div className="stake_center">
+            <div className="title">Step2</div>
+            <div className="text-white">
+              Proceed with delegation to citism.dev according to the staking
+              guide.
+            </div>
+          </div>
+          <div className="stake_right">
+            <div className="title">Step3</div>
+            <div className="text-white">Get rewarded or re-stake again.</div>
+          </div>
         </div>
       </div>
     </>
