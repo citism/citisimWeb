fad7c526e981e599583b6eff718378bd9bcd7848
tetgo <jiungdev@gmail.com>
2022-04-22 17:35:52 +0900

    feat: add network logo

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,5 @@
 import "./card.css";
+import mediImg from "../assets/networks/medibloc/medibloc.png"
 
 export default function Card() {
   return (
@@ -7,7 +8,15 @@ export default function Card() {
       <div class="cardMain">
         <div class="card">
           <div class="cardContent">
-            Mediblock
+            <img src={mediImg} width="50%" />
+            <div>
+              Mediblock
+            </div>
+            <div class="cardViewButton">
+              <a href="https://www.mintscan.io/medibloc/validators/panaceavaloper1gvsgxrcv4uuqgxas5nq538f7hydg46mjx6lvuu">
+                View
+              </a>
+            </div>
           </div>
         </div>
       </div>
