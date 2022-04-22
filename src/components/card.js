17770bf79654849ee73948f9f06bc0c5db453e84
mijin <>
2022-04-23 03:05:34 +0900

    add: design fix

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import "./card.css";
 import "tailwindcss/tailwind.css";
 import mediImg from "../assets/networks/medibloc/medibloc.png";
+import { motion } from "framer-motion";
 
 export default function Card() {
   return (
@@ -11,15 +12,24 @@ export default function Card() {
           <div class="cardContent">
             <img src={mediImg} width="40%" />
             <div className="text-m mt-2">Mediblock</div>
-            <div class="cardViewButton">
-              <a
-                className="text-xs"
-                href="https://www.mintscan.io/medibloc/validators/panaceavaloper1gvsgxrcv4uuqgxas5nq538f7hydg46mjx6lvuu"
-                target="_blank"
+
+            <a
+              href="https://www.mintscan.io/medibloc/validators/panaceavaloper1gvsgxrcv4uuqgxas5nq538f7hydg46mjx6lvuu"
+              target="_blank"
+            >
+              <motion.div
+                whileTap={{ scale: 0.9 }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: 0.2,
+                  },
+                }}
+                className="cardViewButton"
               >
                 View
-              </a>
-            </div>
+              </motion.div>
+            </a>
           </div>
         </div>
       </div>
