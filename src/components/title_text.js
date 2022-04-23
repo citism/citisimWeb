e370259aa730a3e7437f44f17bdd4cb7e2f8d330
mijin <>
2022-04-23 17:46:20 +0900

    fix:style bug, link path

--- src/components/title_text.js
+++ src/components/title_text.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 export default function Text() {
   return (
     <div
-      className={classNames("glitch", "md:text-6xl text-3xl")}
+      className={classNames("glitch", "md:text-6xl text-2xl")}
       width="100%"
       height="100%"
       data-text="THE CITISM TEAM"
